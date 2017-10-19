5a6e375381252cd343d5f78d74454033a423ad91
LiangShuai <ls_forever2013@163.com>
2017-10-19 14:52:10 +0800

    1.1

--- fuxiSSM/src/main/java/com/baizhi/dao/NodeDao.java
+++ fuxiSSM/src/main/java/com/baizhi/dao/NodeDao.java
@@ -1,8 +1,13 @@
 package com.baizhi.dao;
 
+import com.baizhi.entity.Node;
+
+import java.util.List;
+
 /**
  * create by LiangS
  * on 2017/10/18
  */
 public interface NodeDao {
+    List<Node> getAllNodes(int start,int size);
 }
